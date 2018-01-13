c3d1994b1e544dba4dcf0a3083dee0e28e9dc12c
liscju <piotr.listkiewicz@gmail.com>
2018-01-13 12:02:44 +0000

    Add foo_ci module

---
+++ foo_pci/foo_pci.c
@@ -0,0 +1,135 @@
+#include <sys/param.h>
+#include <sys/module.h>
+#include <sys/kernel.h>
+#include <sys/systm.h>
+
+#include <sys/conf.h>
+#include <sys/uio.h>
+#include <sys/bus.h>
+
+#include <dev/pci/pcireg.h>
+#include <dev/pci/pcivar.h>
+
+struct foo_pci_softc { // 1
+	device_t    device; // 2
+	struct cdev *cdev; // 3
+};
+
+static d_open_t  foo_pci_open;
+static d_close_t foo_pci_close;
+static d_read_t  foo_pci_read;
+static d_write_t foo_pci_write;
+
+static struct cdevsw foo_pci_cdevsw = { // 4
+	.d_version   = D_VERSION,
+	.d_open      = foo_pci_open,
+	.d_close     = foo_pci_close,
+	.d_read      = foo_pci_read,
+	.d_write     = foo_pci_write,
+	.d_name      = "foo_pci"
+};
+
+static devclass_t foo_pci_devclass; // 5
+
+static int
+foo_pci_open(struct cdev *dev, int oflags, int devtype, struct thread* td) {
+	struct foo_pci_softc *sc;
+
+	sc = dev->si_drv1;
+	device_printf(sc->device, "opened succesfully\n");
+	return (0);
+}
+
+static int
+foo_pci_close(struct cdev *dev, int fflag, int devtype, struct thread* td) {
+	struct foo_pci_softc *sc;
+
+	sc = dev->si_drv1;
+	device_printf(sc->device, "closed\n");
+	return (0);
+}
+
+static int
+foo_pci_read(struct cdev *dev, struct uio *uio, int oflag) {
+	struct foo_pci_softc *sc;
+
+	sc = dev->si_drv1;
+	device_printf(sc->device, "read request = %dB\n", uio->uio_resid);
+	return (0);
+}
+
+static int
+foo_pci_write(struct cdev *dev, struct uio *uio, int oflag) {
+	struct foo_pci_softc *sc;
+
+	sc = dev->si_drv1;
+	device_printf(sc->device, "write request = %dB\n", uio->uio_resid);
+	return (0);
+}
+
+static struct _pcsid {
+	uint32_t   type;
+	const char *desc;
+} pci_ids[] = {
+	{ 0x1234abcd, "RED PCI Widget" },
+	{ 0x4321fedc, "BLU PCI Widget" },
+	{ 0x00000000, NULL }
+};
+
+static int
+foo_pci_probe(device_t dev) {
+	uint32_t type = pci_get_devid(dev);
+	struct _pcsid *ep = pci_ids;
+
+	while (ep->type && ep->type != type)
+		ep++;
+	if (ep->desc) {
+		device_set_desc(dev, ep->desc);
+		return (BUS_PROBE_DEFAULT);
+	}
+
+	return (ENXIO);
+}
+
+static int
+foo_pci_attach(device_t dev) {
+	struct foo_pci_softc *sc = device_get_softc(dev);
+	int unit = device_get_unit(dev);
+
+	sc->device = dev;
+	sc->cdev = make_dev( // 6
+		&foo_pci_cdevsw
+		, unit
+		, UID_ROOT
+		, GID_WHEEL
+		, 0600
+		, "foo_pci%d"
+		, unit
+	);
+	sc->cdev->si_drv1 = sc;
+
+	return (0);
+}
+
+static int
+foo_pci_detach(device_t dev) {
+	struct foo_pci_softc *sc = device_get_softc(dev);
+
+	destroy_dev(sc->cdev);
+	return (0);
+}
+
+static device_method_t foo_pci_methods[] = {
+	DEVMETHOD(device_probe, foo_pci_probe),
+	DEVMETHOD(device_attach, foo_pci_attach),
+	DEVMETHOD(device_detach, foo_pci_detach),
+	{ 0, 0 }
+};
+
+static driver_t foo_pci_driver = {
+	"foo_pci",
+	foo_pci_methods,
+	sizeof(struct foo_pci_softc)
+};
+
+DRIVER_MODULE(foo_pci, pci, foo_pci_driver, foo_pci_devclass, 0, 0);
